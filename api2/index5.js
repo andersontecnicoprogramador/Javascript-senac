951ecbc97b2bc931279110b703b0b35fce450eeb
andersontecnicoprogramador <anderson.programador@hotmail.com>
2024-03-01 22:52:59 -0300

    oitavo commit

--- api2/index5.js
+++ api2/index5.js
@@ -1,14 +1,14 @@
 fetch('https://my-json-server.typicode.com/professornelcimariano/lista_js/db')
     .then(response => response.json())
-then(data => {
+    .then(data => {
     const lista = printVideos(data);
     const listaContainer = document.getElementById("lista-videos");
-    listaContainer.innerHTML = lista.join("");
+    listaContainer.innerHTML = lista.join('');
 })
     .catch(error => console.error('Erro ao buscar os Dados:', error));
 function printVideos(data) {
     if (data && data.videos) {
-        return data.videos.map(video => '<div>${video.titulo}</div>');
+        return data.videos.map(videos => '<div>${video.titulo}</div>');
     } else {
         console.error('Não foram encontrados videos na resposta');
         return [];
