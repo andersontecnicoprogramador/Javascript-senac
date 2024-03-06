c80cc2e8166a65e74ffc5c9226ec0d3547413795
Anderson Moreira da Silva <anderson.tecnicoprogramdor@hotmail.com>
2024-03-06 16:38:48 -0300

    decimo commit

--- api2/index5.js
+++ api2/index5.js
@@ -1,14 +1,15 @@
 fetch('https://my-json-server.typicode.com/professornelcimariano/lista_js/db')
     .then(response => response.json())
     .then(data => {
-    const lista = printVideos(data);
-    const listaContainer = document.getElementById("lista-videos");
-    listaContainer.innerHTML = lista.join('');
-})
+        const lista = printVideos(data);
+        const listContainer = document.getElementById("lista-videos");
+        listContainer.innerHTML = lista.join("");
+    })
     .catch(error => console.error('Erro ao buscar os Dados:', error));
+
 function printVideos(data) {
     if (data && data.videos) {
-        return data.videos.map(videos => '<div>${video.titulo}</div>');
+        return data.videos.map(video => `<div>${video.titulo}</div>`);
     } else {
         console.error('Não foram encontrados videos na resposta');
         return [];
