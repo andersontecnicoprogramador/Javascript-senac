cf658f41e4091b99b3fe5804ea0afe7931e87435
Anderson Moreira da Silva <anderson.tecnicoprogramdor@hotmail.com>
2024-03-06 17:23:03 -0300

    decimo primeiro commit

--- login/index4.js
+++ login/index4.js
@@ -1,9 +1,9 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
+document.getElementById('formulario').addEventListener('submit', function(event) {
     event.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     // Aqui você pode adicionar a lógica de autenticação
     console.log('Username:', username);
     console.log('Password:', password);
-    console.log('Deu tudo certo:', );
+    console.log('Deu tudo certo:' );
   });
